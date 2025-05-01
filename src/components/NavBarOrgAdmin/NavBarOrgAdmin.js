4b8c5f5f5c36c0011aadfa616f1d38272d56abe0
Roshana Isuranga <roshanaisuran2002@gmail.com>
2025-05-01 16:53:27 +0530

    Added profile icon to the navbar

--- src/components/NavBarOrgAdmin/NavBarOrgAdmin.js
+++ src/components/NavBarOrgAdmin/NavBarOrgAdmin.js
@@ -7,6 +7,7 @@ import "./NavBarOrgAdmin.css";
 const NavBarOrgAdmin = () => {
     const [showAlerts, setShowAlerts] = useState(false);
     const [showNotifications, setShowNotifications] = useState(false);
+    const [showProfile, setShowProfile] = useState(false);
 
     const location = useLocation();
     const isConfigActive = location.pathname.startsWith("/admin/config");
@@ -20,6 +21,12 @@ const NavBarOrgAdmin = () => {
         setShowNotifications(!showNotifications);
         setShowAlerts(false);
     };
+
+    const toggleProfile = () => {
+        setShowNotifications(false);
+        setShowAlerts(false);
+        setShowProfile(!showProfile);
+    }
 
     return (
         <div className="layout-container">
@@ -217,12 +224,29 @@ const NavBarOrgAdmin = () => {
                             </div>
                         )}
                     </div>
-                    <div className="profile-icon">
-                        <img
-                            src="/profile.png"
-                            alt="profile"
-                            className="avatar"
-                        />
+                    <div className="icon-wrapper" onClick={toggleProfile}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="profile-icon"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                            <path
+                                fill-rule="evenodd"
+                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                            />
+                        </svg>
+                        {showProfile && (
+                            <div className="popup-box">
+                                My profile
+
+                                {/* ADD IMAGE, NAME, My profile (link), Log out (link) here */}
+
+                            </div>
+                        )}
                     </div>
                 </div>
             </nav>
